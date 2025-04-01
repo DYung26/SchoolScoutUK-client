28e98b696ec69d8674bd61436322b4c60f367a05
DYung26 <theadstore1407@gmail.com>
2025-04-01 21:12:42 +0100

    enhance `query client` with auth headers and add a reusable `mutation function` for `API` requests.

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -4,8 +4,14 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       queryFn: async ({ queryKey }) => {
+	const user = JSON.parse(localStorage.getItem("user") as string);
+	const token = user["data"]["accessToken"];
         const res = await fetch(queryKey[0] as string, {
           credentials: "include",
+	  headers: {
+	    "Content-Type": "application/json",
+	    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+	  },
         });
 
         if (!res.ok) {
@@ -28,3 +34,26 @@ export const queryClient = new QueryClient({
     }
   },
 });
+
+export async function mutationFn({
+  url,
+  method = "POST",
+  body,
+}: {
+  url: string,
+  method: "POST" | "PUT" | "DELETE",
+  body: any,
+}) {
+  const res = await fetch(url, {
+    method,
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: body ? JSON.stringify(body) : undefined,
+  });
+
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${await res.text()}`);
+  }
+
+  return res.json();
+}
