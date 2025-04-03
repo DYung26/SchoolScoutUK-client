3611e87f501149e0e933df23dce5bf8bb02d13ac
DYung26 <theadstore1407@gmail.com>
2025-04-03 09:04:53 +0100

    feat(components): create `AuthNavbar` component with basic layout for `SchoolScoutUK`

---
+++ src/components/AuthNavbar.tsx
@@ -0,0 +1,11 @@
+
+
+export function AuthNavbar() {
+  return (
+    <nav className="sticky top-0 z-50 w-full h-16 border-b bg-white flex items-center px-4">
+      <h1>SchoolScoutUK</h1>
+    </nav>
+    /*{<nav className="sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60">
+    </nav>}*/
+  );
+}
