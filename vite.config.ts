d657d6f5fe09b3a6500262398b341c7ff41f8f52
DYung26 <theadstore1407@gmail.com>
2025-04-04 00:05:53 +0100

    Removed unnecessary `'root'` configuration in `vite.config.ts` for default project directory handling and adjusted build output directory configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     // port: 5173,
     open: true, // Opens browser on start
   },
-  root: path.resolve(__dirname),
+  // root: path.resolve(__dirname),
   build: {
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
